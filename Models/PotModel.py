3ccc539fb73bb4a0b22e1e3be61ba985f81eb38f
Viktor Máni Mønster <viktor@mani.monster>
2025-09-17 07:52:35 +0200

    Removed whitespace from end of pot adds

--- Models/PotModel.py
+++ Models/PotModel.py
@@ -6,11 +6,13 @@ class PotModel(ModelBase):
         super().__init__(guild, "pot")
     
     def add_pot(self, element: str):
+        element = element.strip()
         if not element in self.data and not element.lower() == "mulligan":
             self.data.append(element)
             self._save_data()
     
     def rem_pot(self, element: str):
+        element = element.strip()
         if element in self.data:
             self.data.remove(element)
             self._save_data()
